a0e74cd07335a82dbc2c6bb942e7606cbbf0b29a
khushalkhan <khushalkhanpaktiawal321@gmail.com>
2023-06-24 21:18:27 +0300

    first commit

---
+++ src/test/Main.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i=0; i<5; i++){
+            System.out.println("This is our first jenkins code, Hello Batch 1");
+        }
+    }
+}
