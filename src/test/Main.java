9d2ec162c390b5dcc9d6495e63c5ab6e44dcc1fd
khushalkhan <khushalkhanpaktiawal321@gmail.com>
2023-06-27 02:53:26 +0300

    second commit

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i=0; i<5; i++){
             System.out.println("This is our first jenkins code, Hello Batch 1");
+            System.out.println("This is my second commit");
         }
     }
 }
